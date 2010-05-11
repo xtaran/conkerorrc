347e7e7dda80ebee34970e619ad543c94b33becb
Axel Beckert <abe@deuxchevaux.org>
2010-05-11 15:49:29 +0200

    Add retroj's delete feature

--- conkerorrc.js
+++ conkerorrc.js
@@ -171,3 +171,13 @@ interactive("set-proxy-type", "Change the proxy type or turn proxies off",
     user_pref("network.proxy.type", proxytype);
   }
 );
+
+// From http://jjfoerch.com/git/conkerorrc/commands.js
+interactive("delete", null,
+    function (I) {
+        var elem = yield read_browser_object(I);
+        elem.parentNode.removeChild(elem);
+    },
+    $browser_object = browser_object_dom_node);
+
+define_key(content_buffer_normal_keymap, "d", "delete");
