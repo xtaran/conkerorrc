23579459a3c4bd511d5050c0f82b74a4ee29a047
Axel Beckert <abe@deuxchevaux.org>
2011-03-23 14:59:50 +0100

    Modeline Counters

--- conkerorrc.js
+++ conkerorrc.js
@@ -30,6 +30,8 @@ read_buffer_show_icons = true;
 
 // favicons hook
 add_hook("mode_line_hook", mode_line_adder(buffer_icon_widget), true);
+add_hook("mode_line_hook", mode_line_adder(loading_count_widget), true);
+add_hook("mode_line_hook", mode_line_adder(buffer_count_widget), true);
 
 //browser_prevent_automatic_form_focus_mode(true);
 google_search_bind_number_shortcuts();
