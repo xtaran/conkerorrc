8e652d00d5b1276a959bd760c2b32df69b6b7e00
Axel Beckert <beckert@phys.ethz.ch>
2011-10-25 16:44:21 +0200

    Lazy Loading

--- conkerorrc.js
+++ conkerorrc.js
@@ -28,6 +28,57 @@ view_source_use_external_editor = false;
 xkcd_add_title = true;
 read_buffer_show_icons = true;
 
+// Delayed session load
+/*
+
+From http://retroj.net/git/conkerorrc/content-delay.js
+
+This script is a hack that provides delayed loading for content buffers.
+The initial url of a buffer will not be loaded until that buffer is
+switched to.  Precaution is taken that the buffer's display_uri_string
+returns the delayed url, not about:blank, so things like tabs and sessions
+will still work properly.
+
+*/
+
+function content_delay (spec) {
+    this.delayed_load = spec;
+}
+
+function content_delay_init (b) {
+    if (b != b.window.buffers.current &&
+        b instanceof content_buffer)
+    {
+        b.load = content_delay;
+        b.__defineGetter__("display_uri_string",
+            function () {
+                if (this.delayed_load) {
+                    if (this.delayed_load instanceof load_spec)
+                        return load_spec_uri_string(this.delayed_load);
+                    return this.delayed_load;
+                }
+                if (this._display_uri)
+                    return this._display_uri;
+                if (this.current_uri)
+                    return this.current_uri.spec;
+                return "";
+            });
+    }
+}
+
+function content_delay_do_initial_load (b) {
+    if (b.hasOwnProperty("load")) {
+        delete b.load;
+        if (b.hasOwnProperty("delayed_load")) {
+            b.load(b.delayed_load);
+            delete b.delayed_load;
+        }
+    }
+}
+
+add_hook("create_buffer_early_hook", content_delay_init);
+add_hook("select_buffer_hook", content_delay_do_initial_load);
+
 // favicons hook
 add_hook("mode_line_hook", mode_line_adder(buffer_icon_widget), true);
 add_hook("mode_line_hook", mode_line_adder(loading_count_widget), true);
